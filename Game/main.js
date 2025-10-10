6cb5a717766d49a50acb61a620f4c1f0b1996609
Daria Vasylieva <daria.vasylieva@ivkhk.ee>
2025-10-10 19:55:44 +0300

    new game

---
+++ Game/main.js
@@ -0,0 +1,21 @@
+//Импортируем необходимые модули
+
+const readLineSync = recuire('rqadline-sync');
+//Контроллер игрового цикла
+
+let keepPlaying = true;
+
+//Основной цикл меню.Будет продолжаться, пока игрок не захочет выйти
+
+while (keepPlaying) {
+    console.clear();
+    console.log("=============================");
+    console.log("=   WELKOME TO THE GAME     =");
+    console.log("=============================");
+    console.log("Which game mode do you want to play?");
+
+
+    const games = [
+        'Guess the Number'
+    ];
+}
